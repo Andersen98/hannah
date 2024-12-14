49bdf7f9b8726e6dc28b95faddb1d55a2ef8d531
Hannah Andersen <thehumanme@gmail.com>
2024-12-14 18:04:44 -0500

    jj to escape insert mode (kakoune)

--- home/kakoune/default.nix
+++ home/kakoune/default.nix
@@ -5,6 +5,10 @@
   programs.kakoune = {
     enable = true;
     extraConfig = ''
+      hook global InsertChar j %{ try %{
+        exec -draft hH <a-k>jj<ret> d
+        exec -with-hooks <esc>
+      }}
       eval %sh{uwsm-app -s b kak-lsp}
       lsp-enable
       '';
