f23729f98d4b7335d0a78d4eed0072cece94a8d6
Hannah Andersen <thehumanme@gmail.com>
2024-12-16 10:29:16 -0500

    changed uwsm path for hyprland to ${pkgs.hyprland}/bin/Hyprland. This is not reccomended but....my system seemed to no longer link hyprland to the location it used to be at. so screw it!

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -21,7 +21,7 @@ programs.uwsm.waylandCompositors = {
       hyprland = {
       prettyName = "Hyprland";
       comment = "Hyprland compositor managed by UWSM";
-      binPath = "/run/current-system/sw/bin/Hyprland";
+      binPath = "${pkgs.hyprland}/bin/Hyprland";
     };
   };
 }
