3fab9414a5464442ec064ff700a451b54b892e66
Hannah Andersen <thehumanme@gmail.com>
2025-01-11 17:54:29 -0500

    here

--- nixos/base/firewall.nix
+++ nixos/base/firewall.nix
@@ -1,8 +1,1 @@
-{networking.firewall = {
-  enable = true;
-  allowedTCPPorts = [ 80 443 6065 ];
-  allowedUDPPortRanges = [
-    { from = 4000; to = 4007; }
-    { from = 8000; to = 8010; }
-  ];
-};}
+{networking.firewall.enable = false;}
